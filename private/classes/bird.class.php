2ab87e73f556fa75c84fb812b261120edf597e63
Prizm Moon <prdigitalmoon@gmail.com>
2024-11-13 00:18:22 -0500

    Adds active record code to bird class

--- private/classes/bird.class.php
+++ private/classes/bird.class.php
@@ -6,6 +6,66 @@ class Bird extends DatabaseObject
   static protected $table_name = 'birds';
   static protected $db_columns = ['id', 'common_name', 'habitat', 'food', 'conservation_id', 'backyard_tips'];
 
+  // ------------ START OF ACTIVE RECORD CODE ------------
+  static protected $database;
+
+  static public function set_database($database)
+  {
+    self::$database = $database;
+  }
+
+  static public function find_by_sql($sql)
+  {
+    $result = self::$database->query($sql);
+
+    if ($result) {
+      exit("Database query failed.");
+    }
+
+    // results into objects
+    $object_array = [];
+    while ($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+
+    $result->free();
+
+    return $object_array;
+  }
+
+  static public function find_all()
+  {
+    $sql = "SELECT * FROM birds";
+    return self::$database->query($sql);
+  }
+
+  static public function find_by_id($id)
+  {
+    $sql = "SELECT * FROM bicycles ";
+    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if (!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
+  }
+
+  static protected function instantiate($record)
+  {
+    $object = new self;
+    // Could automatically assign values to properties
+    // but automatic assignment is easier and re-usable
+    foreach ($record as $property => $value) {
+      if (property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+
+    return $object;
+  }
+  // ------------ END OF ACTIVE RECORD CODE ------------  
+
   public $id;
   public $common_name;
   public $habitat;
